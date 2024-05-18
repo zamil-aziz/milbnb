2f150761b5de60c82351750d6259f5afb7bc2a5b
Zamil <zamilaziz92@gmail.com>
2024-05-18 23:54:34 +0800

    complete 5:19

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
 import Image from "next/image";
+import Container from "./Components/Container";
+import EmptyState from "./Components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./Components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState showReset/>
+    )
+  }
+  
   return (
-    <div className="text-rose-500 text-5xl">Hello AirBNB!</div>
+    <Container>
+      <div 
+        className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing: any) =>{
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          )
+        })}
+      </div>
+    </Container>
+
   );
 }
